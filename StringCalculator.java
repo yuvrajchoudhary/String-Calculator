c0e6f31b3ace7d42255d00d3ae30c1c60d5cc1a1
Yuvraj <yuvraj.choudhary@tavant.com>
2020-06-16 08:17:15 +0530

    Step 3 Completed

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,22 +1,45 @@
 package main;
 
+
 public class StringCalculator {
 
 	int Add(String numbers) {
 		int sum = 0;
 		
-		/**
-		 * step 2 completed
-		 */
-		String[] commaSeparatedNumbers = numbers.split(",");
-		for (String str : commaSeparatedNumbers) {
-			if (str.equalsIgnoreCase(""))
-				sum = sum + 0;
-			else
-				sum = sum + Integer.parseInt(str);
+		if(	numbers.contains(",\n") || numbers.contains("\n,")) {
+			// string is NOT ok
+			return 0;
+		}
+		
+		String[] newLinesSeparatedNumbers = numbers.split("\n");
+		
+		for (String str : newLinesSeparatedNumbers) {
+			if(str.contains(",")) {
+				sum = sum + calulateCommaSeparatedNumbers(str);				
+			}
+			else {
+				sum = sum + calculateNumber(str);
+			}
+			
 		}	
 		
 		return sum;
+	}
+	
+	private int calulateCommaSeparatedNumbers (String str) {
+		int calValue = 0;
+		String[] commaSeparatedNumbers = str.split(",");
+		for (String newStr : commaSeparatedNumbers) {
+			calValue = calValue + calculateNumber(newStr);
+		}		
+		return calValue;		
+	}
+
+	private int calculateNumber(String num) {
+		if (num.equalsIgnoreCase(""))
+			return 0;
+		else
+			return Integer.parseInt(num);
 	}
 
 }
